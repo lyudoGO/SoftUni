b5fdf99279cd3e24355caca7324fbd6bf35ea702
Lyudmil <llgg2312@gmail.com>
2014-09-06 23:44:17 +0300

    Revert "Remove"
    This reverts commit b7364ac55ecc7bf060654873870fc6b8dade525b.

--- Java-Basics/Intro-Java-Homework/run.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-java -jar Deck-of-Cards.jar 
-
--- Java-Basics/Intro-Java-Homework/src/03-PrintHometown.java
+++
@@ -1,9 +0,0 @@
-
-public class PrintHometown {
-
-	public static void main(String[] args) {
-		System.out.println("My town is Sofia");
-
-	}
-
-}

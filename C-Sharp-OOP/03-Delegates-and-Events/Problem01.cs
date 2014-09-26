9d1ac9eeaaf951f3140bdcf12a548dbfd407c49a
Lyudmil <llgg2312@gmail.com>
2014-09-26 18:18:39 +0300

    Adding new tasks

---
+++ C-Sharp-OOP/03-Delegates-and-Events/Problem01.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem01InterestCalculator
+{
+    class InterestCalculator
+    {
+        public delegate decimal CalculateInterest(decimal sumMoney, double interest, int years);
+
+        private decimal sumMoney;
+        private double interest;
+        private int years;
+        private decimal delegateValue;
+
+        public InterestCalculator(decimal sumMoney, double interest, int years, CalculateInterest d)
+        {
+            this.SumMoney = sumMoney;
+            this.Interest = interest;
+            this.Years = years;
+            this.DelegateValue = d(sumMoney, interest, years);
+        }
+
+        public decimal SumMoney
+        {
+            get;
+            set;
+        }
+
+        public double Interest
+        {
+            get;
+            set;
+        }
+
+        public int Years
+        {
+            get;
+            set;
+        }
+
+        public decimal DelegateValue
+        {
+            get;
+            set;
+        }
+
+        public static decimal GetSimpleInterest(decimal sum, double interest, int years)
+        {
+            interest = interest / 100;
+            decimal result = sum * (decimal)(1 + interest * years);
+            return result;
+        }
+
+        public static decimal GetCompoundInterest(decimal sum, double interest, int years)
+        {
+            int n = 12;
+            interest = interest / 100;
+            decimal result = sum * (decimal)Math.Pow((double)(1 + interest / n ), years * n);
+            return result;
+        }
+    }
+
+    class Problem01
+    {
+        static void Main(string[] args)
+        {
+            InterestCalculator compound = new InterestCalculator(500M, 5.6, 10, InterestCalculator.GetCompoundInterest);
+            Console.WriteLine("Compound interset: {0:0.0000}$", compound.DelegateValue);
+
+            InterestCalculator simple = new InterestCalculator(2500M, 7.2, 15, InterestCalculator.GetSimpleInterest);
+            Console.WriteLine("Simple interest: {0:0.0000}$", simple.DelegateValue);
+        }
+    }
+}
---
+++ C-Sharp-OOP/03-Other-Types-in-OOP/Problem02.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Problem02FractionCalculator
+{
+    struct Fraction
+    {
+        private long numerator;
+        private long denominator;
+
+        public Fraction(long numerator, long denominator) : this()
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
+        }
+
+        public long Numerator
+        {
+            get { return this.numerator; }
+            set { this.numerator = value;}
+        }
+
+        public long Denominator
+        {
+            get { return this.denominator; }
+            set 
+            {
+                if (value == 0)
+                {
+                    throw new DivideByZeroException("The denominator cannot be zero!You cannot division by zero!");
+                }
+               
+                this.denominator = value;
+            }
+        }
+
+        public static Fraction operator +(Fraction num1, Fraction num2)
+        {
+            long numeratorResult = num1.Numerator * num2.Denominator + num2.Numerator * num1.Denominator;
+            long denominatorResult = num1.Denominator * num2.Denominator;
+            return new Fraction(numeratorResult, denominatorResult);
+        }
+
+        public static Fraction operator -(Fraction num1, Fraction num2)
+        {
+            long numeratorResult = num1.Numerator * num2.Denominator - num2.Numerator * num1.Denominator;
+            long denominatorResult = num1.Denominator * num2.Denominator;
+            return new Fraction(numeratorResult, denominatorResult);
+        }
+
+        public override string ToString()
+        {
+            decimal result = this.Numerator / this.Denominator;
+            return string.Format("{0}", result);
+        }
+    }
+
+    class Problem02
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction(22, 7);
+            Fraction fraction2 = new Fraction(40, 4);
+            Fraction result = fraction1 + fraction2;
+            Console.WriteLine(result.Numerator);
+            Console.WriteLine(result.Denominator);
+            Console.WriteLine(result);
+
+            // Throw excaption
+            Fraction fraction3 = new Fraction();
+            fraction3.Numerator = 92233720368547758;
+            fraction3.Denominator = 0;
+        }
+    }
+}

f8d13b3b8efe6cc4bd97f5bde4c606b2369dbf74
Lyudmil <llgg2312@gmail.com>
2014-12-17 13:51:18 +0200

    New course

---
+++ JavaScript-AngularJS/01-IntroductionToAngularJS/01.StudentPage/js/students.js
@@ -0,0 +1,17 @@
+function studentController($scope) {
+    $scope.students = [{
+							name: "Pesho",
+							photo: "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+							grade: 5,
+							school: "High School of Mathematics",
+							teacher: "Gichka Pesheva"
+						},
+						{
+							name: "Ani",
+							photo: "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+							grade: 6,
+							school: "High School of Mathematics",
+							teacher: "Penka Zaharieva"
+						}
+	];
+}
